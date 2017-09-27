8e6cb105ce3512f54864d1717343e98621d9d16e
Zongan <651896694@qq.com>
2017-09-27 17:57:35 +0800

    9/27 17:57

--- examples/sketch/main.py
+++ examples/sketch/main.py
@@ -3,37 +3,37 @@
 Created on 2017/9/26
 @author:zongan
 """
-import wx
+import wx,os
 
 
 class SketchWindow(wx.Window):
-    def __init__(self, parent, id):
-        wx.Window.__init__(self, parent, id)
+    def __init__(self,parent,id):
+        wx.Window.__init__(self,parent,id)
         self.SetBackgroundColour('White')
         self.color = (0,0,0,1)  # 画笔颜色
         self.thickness = 5  # 画笔粗细
-        self.pen = wx.Pen(self.color, self.thickness, wx.PENSTYLE_SOLID)  # 创建一只画笔
+        self.pen = wx.Pen(self.color,self.thickness,wx.PENSTYLE_SOLID)  # 创建一只画笔
 
         self.lines = []
         self.curLine = []
-        self.pos = (0, 0)
+        self.pos = (0,0)
 
         #   初始化画布
         self.InitBuffer()
 
         #  绑定事件处理函数
-        self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
-        self.Bind(wx.EVT_LEFT_UP, self.OnLeftUp)
-        self.Bind(wx.EVT_MOTION, self.OnMotion)
-        self.Bind(wx.EVT_SIZE, self.OnSize)
-        self.Bind(wx.EVT_IDLE, self.OnIdle)
-        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_LEFT_DOWN,self.OnLeftDown)
+        self.Bind(wx.EVT_LEFT_UP,self.OnLeftUp)
+        self.Bind(wx.EVT_MOTION,self.OnMotion)
+        self.Bind(wx.EVT_SIZE,self.OnSize)
+        self.Bind(wx.EVT_IDLE,self.OnIdle)
+        self.Bind(wx.EVT_PAINT,self.OnPaint)
 
     def InitBuffer(self):
         '''初始化画布'''
         size = self.GetClientSize()
-        self.buffer = wx.EmptyBitmap(size.width, size.height)
-        dc = wx.BufferedDC(None, self.buffer)  # 设备上下文
+        self.buffer = wx.EmptyBitmap(size.width,size.height)
+        dc = wx.BufferedDC(None,self.buffer)  # 设备上下文
         dc.SetBackground(wx.Brush(self.GetBackgroundColour()))  # 设置画布背景
         dc.Clear()  # 清除画布
         self.DrawLines(dc)
@@ -42,38 +42,38 @@ class SketchWindow(wx.Window):
     def GetLinesData(self):
         return self.lines[:]
 
-    def SetLinesData(self, lines):
+    def SetLinesData(self,lines):
         self.lines = lines[:]
         self.InitBuffer()
         self.Refresh()
 
-    def DrawLines(self, dc):
-        for colour, thickness, line in self.lines:
-            pen = wx.Pen(colour, thickness, wx.PENSTYLE_SOLID)   # 根据线条的颜色，粗细重新设置画笔
+    def DrawLines(self,dc):
+        for colour,thickness,line in self.lines:
+            pen = wx.Pen(colour,thickness,wx.PENSTYLE_SOLID)  # 根据线条的颜色，粗细重新设置画笔
             dc.SetPen(pen)
             for coords in line:
                 dc.DrawLine(*coords)
 
-    def OnLeftDown(self, event):
+    def OnLeftDown(self,event):
         self.LeftDown = True
         self.curLine = []
         self.pos = event.GetPosition()
         self.CaptureMouse()  # 开始捕捉鼠标
 
-    def OnLeftUp(self, event):
+    def OnLeftUp(self,event):
         self.LeftDown = False
         if self.HasCapture():
-            self.lines.append((self.color, self.thickness, self.curLine))
+            self.lines.append((self.color,self.thickness,self.curLine))
             self.curLine = []
             self.ReleaseMouse()  # 释放鼠标
 
-    def OnMotion(self, event):
+    def OnMotion(self,event):
         if event.Dragging() and self.LeftDown:
-            dc = wx.BufferedDC(wx.ClientDC(self), self.buffer)
-            self.drawMotion(dc, event)
+            dc = wx.BufferedDC(wx.ClientDC(self),self.buffer)
+            self.drawMotion(dc,event)
         event.Skip()
 
-    def drawMotion(self, dc, event):
+    def drawMotion(self,dc,event):
         dc.SetPen(self.pen)
         newPos = event.GetPosition()
         coords = tuple(self.pos) + tuple(newPos)
@@ -81,33 +81,37 @@ class SketchWindow(wx.Window):
         self.curLine.append(coords)
         self.pos = newPos
 
-    def OnSize(self, event):
+    def OnSize(self,event):
         self.reInitBuffer = True
 
-    def OnIdle(self, event):
+    def OnIdle(self,event):
         if self.reInitBuffer:
             self.InitBuffer()
             self.Refresh(False)
 
-    def OnPaint(self, event):
-        dc = wx.BufferedPaintDC(self, self.buffer)
+    def OnPaint(self,event):
+        dc = wx.BufferedPaintDC(self,self.buffer)
 
-    def SetColor(self, color):
+    def SetColor(self,color):
         self.color = color
-        self.pen = wx.Pen(color, self.thickness, wx.PENSTYLE_SOLID)
+        self.pen = wx.Pen(color,self.thickness,wx.PENSTYLE_SOLID)
 
-    def SetThickness(self, num):
+    def SetThickness(self,num):
         self.thickness = num
-        self.pen = wx.Pen(self.color, num, wx.PENSTYLE_SOLID)
+        self.pen = wx.Pen(self.color,num,wx.PENSTYLE_SOLID)
 
 
 class SketchFrame(wx.Frame):
-    def __init__(self, parent):
-        wx.Frame.__init__(self, parent, -1, '画板', size=(800, 600))
-        self.sketch = SketchWindow(self, -1)
-        self.sketch.Bind(wx.EVT_MOTION, self.OnSketchMotion)
+    def __init__(self,parent):
+        wx.Frame.__init__(self,parent,-1,'画板',size=(800,600))
+        self.sketch = SketchWindow(self,-1)
+        self.sketch.Bind(wx.EVT_MOTION,self.OnSketchMotion)
+
+        self.wildcard = "Sketch files(*.sketch)|*.sketch|All files (*.*)|*.*"
+
         self.initStatusBar()
         self.createMenuBar()
+        self.createToolBar()
 
     def initStatusBar(self):
         self.statusbar = self.CreateStatusBar()
@@ -148,32 +152,51 @@ class SketchFrame(wx.Frame):
                 ("保存","保存文件",self.OnSave),
                 ("","",""),
                 ("颜色",(
-                    ("黑","",self.OnColor,wx.ITEM_RADIO),
-                    ("红","",self.OnColor,wx.ITEM_RADIO),
-                    ("绿","",self.OnColor,wx.ITEM_RADIO),
-                    ("蓝","",self.OnColor,wx.ITEM_RADIO)
+                    ("Black","",self.OnColor,wx.ITEM_RADIO),
+                    ("Red","",self.OnColor,wx.ITEM_RADIO),
+                    ("Green","",self.OnColor,wx.ITEM_RADIO),
+                    ("Blue","",self.OnColor,wx.ITEM_RADIO)
                 )),
                 ("","",""),
                 ("退出","Quit",self.OnCloseWindow)
-            ))
+            )),
+            ("编辑",()),
+            ("关于",()),
         ]
 
-    def OnSketchMotion(self, event):
-        self.statusbar.SetStatusText("Pos:%s"%str(event.GetPosition()),0)
-        self.statusbar.SetStatusText("Current Pts:%s"%len(self.sketch.curLine),1)
-        self.statusbar.SetStatusText("Line Count:%s"%len(self.sketch.lines),2)
+    def createToolBar(self):
+        pass
+
+    def OnSketchMotion(self,event):
+        self.statusbar.SetStatusText("Pos:%s" % str(event.GetPosition()),0)
+        self.statusbar.SetStatusText("Current Pts:%s" % len(self.sketch.curLine),1)
+        self.statusbar.SetStatusText("Line Count:%s" % len(self.sketch.lines),2)
         event.Skip()
 
     def OnNew(self):
         pass
-    def OnOpen(self):
-        pass
+
+    def OnOpen(self,event):
+        dlg = wx.FileDialog(self,"打开文件...",os.getcwd(),style=wx.FD_DEFAULT_STYLE,wildcard=self.wildcard)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetPath()
+            self.ReadFile()
+            self.SetTitle(self.title + "--" + self.filename)
+        dlg.Destroy()
+
     def OnSave(self):
         pass
-    def OnColor(self):
-        pass
-    def OnCloseWindow(self):
-        pass
+
+    def OnColor(self,event):
+        menubar = self.GetMenuBar()
+        itemId = event.GetId()
+        item = menubar.FindItemById(itemId)
+        color = item.GetLabel()
+        self.sketch.SetColor(color)
+
+    def OnCloseWindow(self,event):
+        self.Destroy()
+
 
 if __name__ == '__main__':
     app = wx.PySimpleApp()
